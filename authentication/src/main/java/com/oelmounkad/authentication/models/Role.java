c13057afae1be4d086396dce038e3c91b1daf5c6
Elmounkad Oussama <oelmounkad@gmail.com>
2021-01-20 05:55:52 +0100

    added Role Model

---
+++ authentication/src/main/java/com/oelmounkad/authentication/models/Role.java
@@ -0,0 +1,38 @@
+package com.oelmounkad.authentication.models;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.oelmounkad.authentication.models.ERole;
+
+@Document(collection = "roles")
+public class Role {
+  @Id
+  private String id;
+
+  private ERole name;
+
+  public Role() {
+
+  }
+
+  public Role(ERole name) {
+    this.name = name;
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public void setId(String id) {
+    this.id = id;
+  }
+
+  public ERole getName() {
+    return name;
+  }
+
+  public void setName(ERole name) {
+    this.name = name;
+  }
+}
