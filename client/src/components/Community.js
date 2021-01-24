2f14633ce4a0ceceaeb740a6d0919c62f22977b4
Elmounkad Oussama <oelmounkad@gmail.com>
2021-01-24 05:41:45 +0100

    added community Page component

---
+++ client/src/components/Community.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Box } from "@chakra-ui/react"
+
+const Community = () => {
+    return (
+      
+            <Box bg="tomato" alignItems="baseline" w="50%" p={4} color="white" marginLeft={80}>
+            This is the Box
+            </Box>
+        
+    )
+}
+
+export default Community
