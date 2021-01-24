6f8f2d9d398dbcc9e2ac98204eea1410ef4870b7
Elmounkad Oussama <oelmounkad@gmail.com>
2021-01-24 05:40:20 +0100

    edited App js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,25 @@
+import './App.css';
 import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
+import Community from './components/Community';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import AuthState from './context/auth/AuthState'
+import Header from './components/Header';
+
 const App = () => {
   return (
+
     <AuthState>
         <Router>
+          <Header />
               <Switch>
-
+                        {/* Auth */}
                       <Route exact path="/login" component={Login} />
                       <Route exact path="/signup" component={Signup} />
+
+
+                      <Route exact path="/community" component={Community} />
+
 
               </Switch>
         </Router>
