e25c256ec5a1562a410bf30858f11fa4e5d32c28
ElMehdi <mehdi.bazzine@gmail.com>
2021-01-25 21:23:45 +0100

    fixing the profile model

---
+++ models/Profile.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+const Profile = mongoose.model('Profile', {
+    fullName : {
+        type: String,
+        required: true,
+        trim : true
+    },
+    sexe : {
+        type : String,
+        enum: ["Male","Female","Other"],
+        required: true
+    },
+    birthdate : {
+        type: Date,
+        required: false
+    },
+    city : {
+        type: String,
+        required: true
+    },
+    profilePicture : {
+        type: String,
+        required: true
+    },
+    description : {
+        type: String,
+        required: false
+    },
+    portfolio : {
+        type: [String],
+        required : false
+    },
+    languages : {
+        type: [String],
+        required : false
+    },
+    hobbies : {
+        type: String,
+        required : false
+    },
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+module.exports = Profile
+
+
+
