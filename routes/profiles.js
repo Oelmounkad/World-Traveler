0e7dfe879227d76d8bc1b75a7953040bb41206f6
ElMehdi <mehdi.bazzine@gmail.com>
2021-01-26 19:59:41 +0100

    Adding the updating route to the profiles router

--- routes/profiles.js
+++ routes/profiles.js
@@ -85,18 +85,61 @@ router.get ('/:id', async (req, res) => {
 // // @desc    Updates a profile
 // // @access  Public
 
-// router.patch ('/', async (req, res) => {
+router.patch ('/:id', auth, async (req, res) => {
+    const _id = req.params.id
 
-//     try {
-//        
-//     }catch(e) {
-//
-//     }
-// })
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['fullName','birthDate','city','profilePicture','description','languages','hobbies' ]
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update) )
 
-// // @route   DELETE api/profiles
-// // @desc    Deletes a profile
-// // @access  Public
+    if (!isValidOperation) {
+        return res.status(400).send({ error : 'Invalid updates'})
+    }
+
+    try {
+        let profile = await Profile.findById(_id)
+        if(!profile) {
+            return res.status(404).send('Profile does not exist !')
+        }
+        if(profile.user.toString() !== req.sub ) {
+            return res.status(401).send('Not authorized to edit this profile !')
+        }
+
+    const {fullName,birthDate,city,description,languages,hobbies} = req.body
+    const modifications = {}
+
+    if(fullName) modifications.fullName = fullName
+    if(birthDate) modifications.birthDate = birthDate
+    if(city) modifications.city = city
+    if(description) modifications.description = description
+    if(languages) modifications.languages = languages
+    if(hobbies) modifications.hobbies = hobbies
+
+        let img_url = ""
+
+    if(req.body.profilePicture){
+
+         await cloudinary.uploader.upload(req.body.profilePicture)
+        .then((result) => {
+            // Recuperate the url of the image stored
+            img_url = result.secure_url
+            modifications.profilePicture = img_url
+        })
+        .catch((e) => {
+          res.status(500).send({e})
+        })
+    }
+    profile = await Profile.findByIdAndUpdate(_id, {$set : modifications},{new: true})
+    res.send(profile)
+
+    }catch(e) {
+        res.status(500).send(e)
+    }
+})
+
+// @route   DELETE api/profiles
+// @desc    Deletes a profile
+// @access  Public
 
 router.delete ('/:id', auth, async (req, res) => {
     const _id = req.params.id
