ccdd0ccc6d6b220b712d2d79d2a9b4bed144f34a
ElMehdi <mehdi.bazzine@gmail.com>
2021-01-26 10:27:28 +0100

    Creation of the cloudinary configuration

---
+++ utils/cloudinary.js
@@ -0,0 +1,11 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+
+// cloudinary configuration
+cloudinary.config({
+    cloud_name: "oelmounkad",
+    api_key: "154112183673125",
+    api_secret: "jEpvAIqVXaUl70_S6jYbeYA2uCc"
+  });
+
+export default cloudinary
