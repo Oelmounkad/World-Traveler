25b595839a9ea9d33872be731c503750139c7dc5
Elmounkad Oussama <oelmounkad@gmail.com>
2021-01-27 09:44:23 +0100

    changed Jwt signing algorithm

--- authentication/src/main/java/com/oelmounkad/authentication/security/jwt/JwtUtils.java
+++ authentication/src/main/java/com/oelmounkad/authentication/security/jwt/JwtUtils.java
@@ -36,7 +36,7 @@ public class JwtUtils {
 				.setSubject((userPrincipal.getId()))
 				.setIssuedAt(new Date())
 				.setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
-				.signWith(SignatureAlgorithm.HS512, jwtSecret)
+				.signWith(SignatureAlgorithm.HS256, jwtSecret)
 				.compact();
 	}
 
