5d59cfce275c292542e608c46b6882d8c6742e78
Elmounkad Oussama <oelmounkad@gmail.com>
2021-01-27 11:07:35 +0100

    added new types for the context Api

--- client/src/context/types.js
+++ client/src/context/types.js
@@ -3,4 +3,6 @@
 export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
 export const LOGIN_FAIL = 'LOGIN_FAIL'
 export const REGISTER_SUCCESS= 'REGISTER_SUCCESS'
+export const PERSIST_PROFILE = 'PERSIST_PROFILE'
 export const REGISTER_FAIL = 'REGISTER_FAIL'
+export const LOGOUT = 'LOGOUT'
