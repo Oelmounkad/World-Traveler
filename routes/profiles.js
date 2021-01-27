e0d9f82e2e16357405365879da2aa20424126c49
Elmounkad Oussama <oelmounkad@gmail.com>
2021-01-27 09:54:23 +0100

    removed property check to accept any number of props

--- routes/profiles.js
+++ routes/profiles.js
@@ -108,14 +108,6 @@ router.get ('/user/:id', async (req, res) => {
 router.patch ('/:id', auth, async (req, res) => {
     const _id = req.params.id
 
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['fullName','birthDate','city','profilePicture','description','languages','hobbies' ]
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update) )
-
-    if (!isValidOperation) {
-        return res.status(400).send({ error : 'Invalid updates'})
-    }
-
     try {
         let profile = await Profile.findById(_id)
         if(!profile) {
@@ -125,7 +117,7 @@ router.patch ('/:id', auth, async (req, res) => {
             return res.status(401).send('Not authorized to edit this profile !')
         }
 
-    const {fullName,birthDate,city,description,languages,hobbies} = req.body
+    const {fullName,birthDate,city,description,languages,hobbies,profilePicture} = req.body
     const modifications = {}
 
     if(fullName) modifications.fullName = fullName
@@ -137,9 +129,9 @@ router.patch ('/:id', auth, async (req, res) => {
 
         let img_url = ""
 
-    if(req.body.profilePicture){
+    if(profilePicture){
 
-         await cloudinary.uploader.upload(req.body.profilePicture)
+         await cloudinary.uploader.upload(profilePicture)
         .then((result) => {
             // Recuperate the url of the image stored
             img_url = result.secure_url
