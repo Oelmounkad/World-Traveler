64e4ae31193876f38ce5b37b5a0006c1dc292404
ElMehdi <mehdi.bazzine@gmail.com>
2021-01-29 17:18:09 +0100

    Creating the comment model

---
+++ models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const Comment = mongoose.model('Comment',{
+    user: {
+        type: mongoose.Types.ObjectId,
+        ref: 'user'
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    likes: {
+        type: Number,
+        default: 0
+    },
+    likers : [{
+        type : mongoose.Types.ObjectId,
+        ref : 'User'
+    }],
+    date : {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Comment
