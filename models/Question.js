a74a760a48e3b09412f69a571f7e7c8e3237011f
ElMehdi <mehdi.bazzine@gmail.com>
2021-01-29 14:42:21 +0100

    Creating the question model

---
+++ models/Question.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+
+const Question = mongoose.model('Question', {
+    user : {
+        type : mongoose.Types.ObjectId,
+        ref : 'User'
+    },
+    description : {
+        type : String,
+        required : true
+    },
+    pictures : [{
+        type : String
+    }],
+    theme : {
+        type : String,
+        required : true
+    },
+    city : {
+        type : String,
+        required : true
+    },
+    likes : {
+        type : Number,
+        default : 0
+    },
+    likers : [{
+        type : mongoose.Types.ObjectId,
+        ref : 'User'
+    }],
+    comments : [{
+        type : mongoose.Types.ObjectId,
+        ref : 'Comment'
+    }],
+    date : {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = Question
