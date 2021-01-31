0edb0e3582232988459716a7428fcd75b2e6a3c1
Elmounkad Oussama <oelmounkad@gmail.com>
2021-01-31 11:34:52 +0100

    added update / add profile functionnality

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,6 @@
 import React,{useEffect,useContext,useState} from 'react'
-import {Box, Flex,Modal,ModalOverlay,ModalContent,Spinner,ModalCloseButton,ModalBody,ModalFooter, useDisclosure ,Text,Image, Icon, Spacer, Button, Heading} from "@chakra-ui/react"
-import {MdEdit,MdPermContactCalendar,MdPersonAdd,MdChat, MdFavorite, MdGroupWork, MdLanguage, MdFitnessCenter} from 'react-icons/md'
+import {Box ,Input,Flex,Modal,ModalOverlay,ModalContent,Spinner,ModalCloseButton,ModalBody,ModalFooter, useDisclosure ,Text,Image, Icon, Spacer, Button, Heading, Textarea, Select} from "@chakra-ui/react"
+import {MdEdit,MdPermContactCalendar,MdPersonAdd,MdChat, MdFavorite, MdGroupWork, MdLanguage, MdFitnessCenter, MdSave, MdKeyboardReturn} from 'react-icons/md'
 import AppContext from '../context/app/AppContext'
 import AuthContext from '../context/auth/AuthContext'
 import moment from 'moment'
@@ -10,16 +10,34 @@ const Profile = props => {
     const { match: { params } } = props 
 
     const appContext = useContext(AppContext)
-    const {getChosenProfile,chosenProfile} = appContext
+    const {getChosenProfile,chosenProfile,updateProfile} = appContext
 
     const authContext = useContext(AuthContext)
     const {user} = authContext
 
     const [modalImage, setModalImage] = useState('')
+    const [editMode, setEditMode] = useState(false)
+    const [editedProfile,setEditedProfile] = useState({})
+    const [editedBirthday, setEditedBirthday] = useState({
+        day:'',month:'',year:''
+    })
 
     useEffect(() => {
         getChosenProfile(params.id)
     }, [])
+
+    useEffect(() => {
+        if(chosenProfile !== null){
+            setEditedProfile({...editedProfile,
+                ...chosenProfile
+        })
+        let day = moment(chosenProfile.birthDate,'DD-MM-YYYY').date()
+        let month = moment(chosenProfile.birthDate,'DD-MM-YYYY').month()+1
+        let year = moment(chosenProfile.birthDate,'DD-MM-YYYY').year()
+        setEditedBirthday({...editedBirthday, day,month,year })
+
+        }
+    }, [chosenProfile])
 
     const { isOpen, onOpen, onClose } = useDisclosure()
 
@@ -27,6 +45,53 @@ const Profile = props => {
         setModalImage(image)
         onOpen()
     }
+
+    const onChangeEdited = e => {
+        setEditedProfile({...editedProfile,
+                    [e.target.name]:e.target.value
+            })
+    }
+
+    const onChangeSelectLanguage = e => {
+        if(!editedProfile.languages.includes(e.target.value)){
+            setEditedProfile({...editedProfile,
+                    [e.target.name]:[...editedProfile.languages,e.target.value]
+            })
+        }else{
+            setEditedProfile({...editedProfile,
+                [e.target.name]:editedProfile.languages.filter(lang => lang !== e.target.value)
+        })
+        }
+        
+    }
+
+    const onChangeSelectHobby = e => {
+        if(!editedProfile.hobbies.includes(e.target.value)){
+            setEditedProfile({...editedProfile,
+                    [e.target.name]:[...editedProfile.hobbies,e.target.value]
+            })
+        }else{
+            setEditedProfile({...editedProfile,
+                [e.target.name]:editedProfile.hobbies.filter(hob => hob !== e.target.value)
+        })
+        }
+        
+    }
+
+    const onChangeEditedBirthday = e => {
+        setEditedBirthday({...editedBirthday,
+                [e.target.name]:e.target.value
+        })
+    }
+
+    const onSubmit = e => {
+        e.preventDefault()
+        let birthDate = editedBirthday.day+"-"+editedBirthday.month+"-"+editedBirthday.year
+        editedProfile.birthDate = birthDate
+        updateProfile(editedProfile)
+        setEditMode(false)
+    }
+
     return (
         <>
         {chosenProfile !== null ? 
@@ -115,7 +180,7 @@ const Profile = props => {
                            :
                            
                            <Flex direction="row">
-                           <Button marginRight='6px'>
+                           <Button onClick={() => setEditMode(true)} marginRight='6px'>
                                 <Icon color="#C62828" w={8} h={8} as={MdEdit} />
                                 <Text>Edit Profile</Text>
                             </Button>
@@ -157,13 +222,81 @@ const Profile = props => {
                             </ModalContent>
                         </Modal>
 
-                     <Box marginLeft="200">
-                        <Heading>{chosenProfile.fullName}</Heading>
-                       
-                        <Text  fontSize="xl">
-                        {chosenProfile.description}
-                        </Text>
-                     </Box>
+                { editMode ? 
+                
+                <Flex mx='200' direction="column">
+                        {/** Here gooes editing form */}
+                        <form onSubmit={onSubmit}>
+                        <Flex direction="column">
+                             <Heading>Full Name</Heading>
+                             <Input type="text" name="fullName" placeholder="Full Name" value={editedProfile.fullName} onChange={onChangeEdited} />
+                        </Flex>
+                        <Flex direction="column">
+                             <Heading>Bio</Heading>
+                             <Textarea type="text" name="description" placeholder="Bio" value={editedProfile.description} onChange={onChangeEdited} />
+                        </Flex>
+                    
+                    <Flex direction="row">
+                               <Box>
+                                <Heading>Languages</Heading>
+                                <Select name="languages" placeholder="Select option" multiple height='100px' width="300px" value={editedProfile.languages} onChange={onChangeSelectLanguage}>
+                                    <option value="German">German</option>
+                                    <option value="English">English</option>
+                                    <option value="French">French</option>
+                                    <option value="Dutch">Dutch</option>
+                                    <option value="Italian">Italian</option>
+                                </Select>  
+                                </Box>
+
+                                <Spacer />
+
+                                <Box>
+                                <Heading>Hobbies</Heading>
+                                <Select name="hobbies" placeholder="Select option" multiple height='100px' width="300px" value={editedProfile.hobbies} onChange={onChangeSelectHobby}>
+                                    <option value="Football">Football</option>
+                                    <option value="Skydiving">Skydiving</option>
+                                    <option value="Swimming">Swimming</option>
+                                    <option value="Tennis">Tennis</option>
+                                    <option value="Art">Art</option>
+                                </Select>  
+                                </Box>
+
+                    </Flex>
+                        
+                        <Flex direction="column">
+                             <Heading>Address</Heading>
+                             <Textarea type="text" name="address" placeholder="Address" value={editedProfile.address} onChange={onChangeEdited} />
+                        </Flex>
+                        <Box>
+                        <Heading>Birthday</Heading>
+                        <Flex direction="row">
+                        <Input type="number" name="day" placeholder="Day" value={editedBirthday.day} onChange={onChangeEditedBirthday} />
+                        <Input type="number" name="month" placeholder="Month" value={editedBirthday.month} onChange={onChangeEditedBirthday} />
+                        <Input type="number" name="year" placeholder="Year" value={editedBirthday.year} onChange={onChangeEditedBirthday} />
+                        </Flex>
+                        </Box>
+                        <br />
+                        <Button type="submit" marginRight='6px'>
+                                <Icon color="#C62828" w={8} h={8} as={MdSave} />
+                                <Text>Save Changes</Text>
+                        </Button>
+                        <Button onClick={() => setEditMode(false)} marginRight='6px'>
+                                <Icon color="#C62828" w={8} h={8} as={MdKeyboardReturn} />
+                                <Text>Return</Text>
+                        </Button>
+                        </form>
+                </Flex> 
+                
+                    :   
+                
+                            <Box marginLeft="200">
+                                <Heading>{chosenProfile.fullName}</Heading>
+                                <Text  fontSize="xl">
+                                {chosenProfile.description}
+                                </Text>
+                            </Box> 
+                     }
+                     
                       
 
                 </Flex>
