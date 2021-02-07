f1079e9fc7e9f9f72f5d70e70f4f092e81b1f42b
ElMehdi <mehdi.bazzine@gmail.com>
2021-02-07 12:56:35 +0100

    Adding the reading all user's meetings route

--- routes/meetings.js
+++ routes/meetings.js
@@ -28,4 +28,25 @@ router.post('/:id', auth, async (req, res) => {
     }
 })
 
+// GET /api/meetings/
+// @desc READ a user's meetings
+// @access Private
+
+router.get('/', auth, async (req, res) => {
+
+    try {
+        const meetings = await Meeting.find({user_1 : req.sub })
+        const meetingsBis = await Meeting.find({user_2 : req.sub})
+
+        Array.prototype.push.apply(meetings, meetingsBis)
+        
+        if (!meetings) {
+            return res.status(404).send('error : No meeting found')
+        }
+        res.send(meetings)
+     }catch(e) {
+        res.status(500).send(e)
+    }
+})
+
 module.exports = router
