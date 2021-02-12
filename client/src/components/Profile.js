b3927f4db2a7ae38a780131c260506ce57e7c650
Elmounkad Oussama <oelmounkad@gmail.com>
2021-02-12 18:09:59 +0100

    added contact functionnality in Profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,25 +1,30 @@
 import React,{useEffect,useContext,useState} from 'react'
 import {AddIcon, EditIcon} from "@chakra-ui/icons"
-import {Box ,Input,Flex,Modal,ModalOverlay,ModalContent,Spinner,ModalCloseButton,ModalBody,ModalFooter, useDisclosure ,Text,Image, Icon, Spacer, Button, Heading, Textarea, Select, IconButton, Center} from "@chakra-ui/react"
+import {Box,Input,Flex,Modal,ModalOverlay,ModalContent,Spinner,ModalCloseButton,ModalBody,ModalFooter, useDisclosure ,Text,Image, Icon, Spacer, Button, Heading, Textarea, Select, IconButton, Center, FormLabel, FormControl, ModalHeader} from "@chakra-ui/react"
 import {MdEdit,MdPermContactCalendar,MdPersonAdd,MdChat, MdFavorite, MdGroupWork, MdLanguage, MdFitnessCenter, MdSave, MdKeyboardReturn} from 'react-icons/md'
 import AppContext from '../context/app/AppContext'
 import AuthContext from '../context/auth/AuthContext'
 import moment from 'moment'
+import {useHistory} from 'react-router-dom'
 
 const Profile = props => {
 
-    
+    const history = useHistory()
+
+    const [modalSubject, setModalSubject] = useState({})
+    const [message, setMessage] = useState('')
+    const [time, setTime] = useState('')
+
     let hiddenInput = null
     let hiddenInput2 = null
     const { match: { params } } = props 
 
     const appContext = useContext(AppContext)
-    const {getChosenProfile,chosenProfile,updateProfile,addPhotoToPortfolio,editProfilePhoto} = appContext
+    const {getChosenProfile,chosenProfile,updateProfile,addPhotoToPortfolio,editProfilePhoto,requestMeeting} = appContext
 
     const authContext = useContext(AuthContext)
     const {user} = authContext
 
-    const [modalImage, setModalImage] = useState('')
     const [editMode, setEditMode] = useState(false)
     const [editedProfile,setEditedProfile] = useState({})
     const [editedBirthday, setEditedBirthday] = useState({
@@ -47,10 +52,6 @@ const Profile = props => {
 
     const { isOpen, onOpen, onClose } = useDisclosure()
 
-    const handleOpen = image => {
-        setModalImage(image)
-        onOpen()
-    }
 
     const onChangeEdited = e => {
         setEditedProfile({...editedProfile,
@@ -185,6 +186,28 @@ const Profile = props => {
         }
     }, [previewSource]);
 
+    const handleRequestMeeting = e => {
+        e.preventDefault()
+        if(message !== ''){
+          const reg = /^(0?[1-9]|[12][0-9]|3[01])[\/\-](0?[1-9]|1[012])[\/\-]\d{4}$/;
+
+          if(!time.match(reg)){
+            alert('wrong date format !')
+            return;
+          }
+            let data = {
+                location: modalSubject.city,
+                message: message,
+                time: time,
+                statut : "pending"
+            }
+            requestMeeting(modalSubject.user,data)
+            .then(_ => {
+              history.push(`/meetings`)
+            })
+        }else alert('you must enter a message !')
+    }
+
     return (
         <>
         {chosenProfile !== null ? 
@@ -289,7 +312,7 @@ const Profile = props => {
                                
                            </Flex>
                                 {/** Buttons */}
-                        { chosenProfile.user == user.id &&  
+                        { chosenProfile.user == user.id ? 
                            
                            <Flex direction="row">
                            <Button onClick={() => setEditMode(true)} marginRight='6px'>
@@ -301,6 +324,18 @@ const Profile = props => {
                                 <Text>Meetings</Text>
                             </Button>
                            </Flex> 
+                           
+                           :
+
+                            <Center direction="row" mb='2'>
+                            <Button onClick={() => {
+                            setModalSubject(chosenProfile)
+                            onOpen()
+                            } } marginRight='6px'>
+                                <Icon color="#C62828" w={8} h={8} as={MdChat} />
+                                <Text>Contact</Text>
+                            </Button>
+                            </Center> 
                         }
                            
 
@@ -308,7 +343,34 @@ const Profile = props => {
 
                 </Flex>
 
+                <form> <Modal
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        
+        <ModalContent>
+          <ModalHeader>Contact {modalSubject.name} !</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl>
+              <FormLabel>Message</FormLabel>
+              <Textarea value={message}  placeholder="Send a message!" onChange={e => setMessage(e.target.value)} />
+              <FormLabel>Time</FormLabel>
+              <Input value={time}  placeholder="Time of your meeting ! format : (DD-MM-YYY)" onChange={e => setTime(e.target.value)} />
+            </FormControl>
+          </ModalBody>
 
+          <ModalFooter>
+            <Button type="submit" onClick={handleRequestMeeting} colorScheme="blue" mr={3}>
+              Send!
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal></form>
+
+<br />
 
              {/** portfolio + desc */}
 
@@ -361,22 +423,11 @@ const Profile = props => {
                     {chosenProfile.portfolio.length !== 0 && 
                      chosenProfile.portfolio.slice(0).reverse().map(image => 
                      
-                        <Image cursor="pointer" onClick={() => handleOpen(image)}  m='1.5' marginBottom='2' boxSize="250px" src={image} objectFit="cover" />
+                        <Image cursor="pointer"  m='1.5' marginBottom='2' boxSize="250px" src={image} objectFit="cover" />
                     
                      )
                     }
                        </Flex>
-
-                       <Modal isOpen={isOpen} onClose={onClose}>
-                            <ModalOverlay />
-                            <ModalContent>
-                           
-                            <ModalCloseButton />
-                            <ModalBody>
-                            <Image   boxSize="500px" src={modalImage} objectFit="cover" />
-                            </ModalBody>
-                            </ModalContent>
-                        </Modal>
 
                 { editMode ? 
                 
